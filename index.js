cf6df1ef8cc8685ea8d5e2ca1e5d070bef5f19ab
Matt Farmer <mrfarmer777@gmail.com>
2018-07-26 16:09:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function calculatesFarePrice(str,dest){
   } else if(dist<400){
     fare=0;
   } else if(dist<2000){
-    fare=0.02*dist-400;
+    fare=0.02*(dist-400);
   } else {
     fare=25;
   }
