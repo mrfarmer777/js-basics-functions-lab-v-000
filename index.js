caa06175e08251f623515f9d64651c78e376f240
Matt Farmer <mrfarmer777@gmail.com>
2018-07-26 16:09:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pos){
+  return Math.abs(42-pos);
+}
+
+function distanceFromHqInFeet(pos){
+  return distanceFromHqInBlocks(pos)*264;
+}
+
+function distanceTravelledInFeet(str,pos){
+  return Math.abs(str-pos)*264;
+}
+
+function calculatesFarePrice(str,dest){
+  const dist=distanceTravelledInFeet(str,dest);
+  let fare;
+  if (dist>2500){
+    fare="cannot travel that far";
+  } else if(dist<400){
+    fare=0;
+  } else if(dist<2000){
+    fare=0.02*(dist-400);
+  } else {
+    fare=25;
+  }
+  return fare;
+}
